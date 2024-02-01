3afcc9781010f5586b3198d286b42e14e1910e09
Inba4398 <inbavenu04@gmail.com>
2024-02-01 21:15:01 +0530

    first commit

---
+++ OpenBrowser.java
@@ -0,0 +1,24 @@
+package Task13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//setup driver and open browser
+		ChromeDriver driver = new ChromeDriver();
+		//Pass url to load in browser
+		driver. get ("https://www.google.com/");
+		//To maximize window of browser
+		driver.manage().window().maximize();
+		//To locate the search box using locator and enter text to be searched, and to select  enter we used keys tag
+		driver.findElement(By.name("q")).sendKeys("Selenium browser driver",Keys.ENTER);
+		//to close the browser
+		
+	}
+
+}
